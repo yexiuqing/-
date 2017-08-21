9f8b293b18e43c442f230dfb792d4f1970b20c38
yexiuqing <1124934298@qq.com>
2017-08-21 21:51:39 +0800

    index.vue更改未完成

--- AlisaBlog_vue/src/components/Index.vue
+++ AlisaBlog_vue/src/components/Index.vue
@@ -1,6 +1,43 @@
 <template>
   <div id="page">
+    <ul class="head">
+      <li class="person"> + 我的--个人资料</li>
+      <li class="nameTop">叶秀青</li>
+    </ul>
+    <div class="content">
+      <div class="contentL">
+        <ul>
+          <li class="detail">
+            <span class="glyphicon glyphicon-user">姓名</span>
+            <span class="list">叶秀青</span>
+          </li>
+          <li class="detail">
+            <span class="glyphicon glyphicon-user">姓名</span>
+            <span class="list">叶秀青</span>
+          </li>
+          <li class="detail">
+            <span class="glyphicon glyphicon-user">姓名</span>
+            <span class="list">叶秀青</span>
+          </li>
+          <li class="detail">
+            <span class="glyphicon glyphicon-user">姓名</span>
+            <span class="list">叶秀青</span>
+          </li>
+          <li class="detail">
+            <span class="glyphicon glyphicon-user">姓名</span>
+            <span class="list">叶秀青</span>
+          </li>
+          <li class="detail">
+            <span class="glyphicon glyphicon-user">姓名</span>
+            <span class="list">叶秀青</span>
+          </li>
+        </ul>
 
+      </div>
+      <div class="contentR">
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,5 +65,46 @@ export default {
 	top: 1.15rem;
 	left: 1.4rem;
   border: .05rem solid #aed7dd;
+  padding:0 .07rem;
 }
+  #page .head{
+    height:.5rem;
+    border-bottom: .001rem solid #eee;
+    line-height:.25rem;
+    font-size:.1rem;
+    color: #888;
+    overflow: hidden;
+  }
+  #page .person{
+    border-bottom: .001rem solid #eee;
+  }
+  #page .content{
+    display: flex;
+  }
+  #page .contentL{
+    position: relative;
+    flex:1;
+    padding-right:.08rem;
+  }
+
+  #page .nameTop{
+    height:.25rem;
+    border-bottom: .001rem solid #eee;
+    color: #aed7dd;
+  }
+  #page .detail{
+    height:.23rem;
+    line-height:.23rem;
+    border-bottom: .001rem solid #eee;
+  }
+  #page .detail .glyphicon{
+    color: #aed7dd;
+  }
+  #page .detail .list{
+    float: right;
+  }
+  #page .contentR{
+    flex:1;
+  }
+
 </style>
