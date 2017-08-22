db333dc23f57e3765ea2493b67deba1361376769
yexiuqing <1124934298@qq.com>
2017-08-22 23:29:04 +0800

    样式重构3

--- AlisaBlog_vue/src/components/Download.vue
+++ AlisaBlog_vue/src/components/Download.vue
@@ -1,11 +1,20 @@
 <template>
-  <div id="person">
-	
+  <div id="download">
+
   </div>
 </template>
 <script>
 
 </script>
 <style>
-
+#download{
+  height: 3.8rem;
+  width:4.2rem;
+  background: #fff;
+  position: absolute;
+  top: 1.15rem;
+  left: 1.4rem;
+  border: .05rem solid #aed7dd;
+  padding:0 .07rem;
+}
 </style>
