49cbcd62e312c554da0135757e8367fa8c4a25e7
yexiuqing <1124934298@qq.com>
2017-08-24 23:27:15 +0800

    样式重构4

--- AlisaBlog_vue/src/components/Show.vue
+++ AlisaBlog_vue/src/components/Show.vue
@@ -7,5 +7,13 @@
 
 </script>
 <style>
-
+  #tel{
+    height: 3.8rem;
+    width:4.2rem;
+    background: #fff;
+    position: absolute;
+    top: 1.15rem;
+    left: 1.4rem;
+    border: .05rem solid #aed7dd;
+  }
 </style>
